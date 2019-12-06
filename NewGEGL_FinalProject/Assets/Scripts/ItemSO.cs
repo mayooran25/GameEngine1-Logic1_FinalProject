b0b9b4fb98a962969fda3137a801a1a58794fafc
mayooran25 <athithan.mayooran5@gmail.com>
2019-12-06 12:24:06 -0500

    added ItemSO
    added ItemSO

---
+++ NewGEGL_FinalProject/Assets/Scripts/ItemSO.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField]
+public enum BodyPart
+{
+    Head, Body, Weapons, Hands, Feet
+}
+
+
+[System.Serializable]
+
+[CreateAssetMenu(menuName = "ItemSO", fileName = "ItemSO", order = 1)]
+public class ItemSO : ScriptableObject
+{
+    public Sprite itemIcon;
+    public string itemName;
+    public List<GameObject> itemPrefab;
+    public Vector3 position;
+    public Vector3 rotation;
+    public BodyPart BodyPart;
+          
+
+
+}
+

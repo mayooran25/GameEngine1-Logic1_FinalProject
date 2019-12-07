cd8eb253003eae46049f07377b9145d8b6afb3b5
mayooran25 <athithan.mayooran5@gmail.com>
2019-12-07 00:18:37 -0500

    added script UI controller

---
+++ NewGEGL_FinalProject/Assets/Scripts/UIController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+
+    public GameObject ItemPanel;
+    
+
+
+
+
+
+
+
+
+}

dd93b344a7a4f75a840a4ce94898804d8fb7319f
FTWRahul <r.poundarik@gmail.com>
2019-12-08 21:33:02 -0500

    Podium script

---
+++ NewGEGL_FinalProject/Assets/Scripts/Podium.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Podium : MonoBehaviour
+{
+    public GameObject player;
+    public Transform podiumPosition;
+    public Dictionary<BodyPart, List<ItemSO>> items;
+    public List<ItemSO> allList;
+    private UIController _uiScript;
+
+    private void Awake()
+    {
+        InitializeDictonary();
+
+
+        _uiScript = GetComponentInChildren<UIController>();
+    }
+
+    void InitializeDictonary()
+    {
+        items.Add(BodyPart.Body, new List<ItemSO>());
+        items.Add(BodyPart.Feet, new List<ItemSO>());
+        items.Add(BodyPart.Hands, new List<ItemSO>());
+        items.Add(BodyPart.Head, new List<ItemSO>());
+        items.Add(BodyPart.Weapons, new List<ItemSO>());
+        
+        foreach (var item in allList)
+        {
+            items[item.BodyPart].Add(item);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        foreach (var bodyPart in items)
+        {
+            //make panels
+        }
+    }
+}

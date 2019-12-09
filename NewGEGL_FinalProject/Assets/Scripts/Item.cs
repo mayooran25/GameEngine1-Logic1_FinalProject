425983aa45199838f780d702c1799f60a4190b2e
FTWRahul <r.poundarik@gmail.com>
2019-12-08 20:29:59 -0500

    Item Script added

---
+++ NewGEGL_FinalProject/Assets/Scripts/Item.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Item : MonoBehaviour
+{
+    public Image itemIcon;
+    public Text itemNameText;
+
+    public void Init(ItemSO data)
+    {
+        this.itemIcon.sprite = data.itemIcon;
+        this.itemNameText.text = data.name;
+    }
+}

ab0f0199b201f7252778c32cc0ec795722db8f1f
mayooran25 <athithan.mayooran5@gmail.com>
2019-12-08 23:06:57 -0500

    added UI controller

--- NewGEGL_FinalProject/Assets/Scripts/Item.cs
+++ NewGEGL_FinalProject/Assets/Scripts/Item.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Item : MonoBehaviour
+public class item : MonoBehaviour
 {
     public Image itemIcon;
     public Text itemNameText;
--- NewGEGL_FinalProject/Assets/Scripts/UIController.cs
+++ NewGEGL_FinalProject/Assets/Scripts/UIController.cs
@@ -2,11 +2,77 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class UIController : MonoBehaviour
 {
 
-    public GameObject ItemPanel;
     
+    public Transform HeadPanel;
+    public Transform FeetPanel;
+    public Transform WeaponsPanel;
+    public Transform HandsPanel;
+    public Transform BodyPanel;
+ 
+    public void CreatePanelItem( BodyPart bodypart,List<ItemSO> items)
+    {
+        foreach (ItemSO item in items)
+        {
+           if (bodypart == BodyPart.Head)
+            {
+                Instantiate((GetComponent<item>().itemIcon), HeadPanel);
+                Instantiate((GetComponent<item>().itemNameText), HeadPanel);
+
+
+            }
+
+           else
+
+            if (bodypart == BodyPart.Feet)
+            {
+                Instantiate((GetComponent<item>().itemIcon),FeetPanel);
+                Instantiate((GetComponent<item>().itemNameText), FeetPanel);
+
+
+            }
+
+           else
+
+            if (bodypart == BodyPart.Hands)
+            {
+                Instantiate((GetComponent<item>().itemIcon), HandsPanel);
+                Instantiate((GetComponent<item>().itemNameText),HandsPanel);
+
+
+            }
+
+            else
+
+            if (bodypart == BodyPart.Body)
+            {
+                Instantiate((GetComponent<item>().itemIcon), BodyPanel);
+                Instantiate((GetComponent<item>().itemNameText), BodyPanel);
+
+
+            }
+
+            else
+
+            if (bodypart == BodyPart.Weapons)
+            {
+                Instantiate((GetComponent<item>().itemIcon), WeaponsPanel);
+                Instantiate((GetComponent<item>().itemNameText), WeaponsPanel);
+
+
+            }
+
+        }
+       
+
+
+
+
+    }
+
 
 
 

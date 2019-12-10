26dfbf93c908ef72dbdb7e15f98e3f542795b3c2
EyepatchDoge <45701926+EyepatchDoge@users.noreply.github.com>
2019-12-10 00:04:56 -0500

    Main Menu~Kaz
    - created main menu with start, options and quit
    -created script that will move from scene to scene and game if Quit is hit

---
+++ NewGEGL_FinalProject/Assets/Scripts/SceneManaging.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManaging : MonoBehaviour
+{
+    public string newScene;
+    
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(newScene);
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();
+        Debug.Log("Game has quit");
+    }
+}

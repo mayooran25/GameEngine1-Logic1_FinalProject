ed924d89c6320aeb7fec35ecaeba450e90a82324
EyepatchDoge <45701926+EyepatchDoge@users.noreply.github.com>
2019-12-11 16:33:11 -0500

    soundManager~Kaz
    - made a sound manager and a singleton manager

---
+++ NewGEGL_FinalProject/Assets/Scripts/SingletonManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonManager<T> : MonoBehaviour where T : SingletonManager<T>
+{
+    //local variable instance (referencing itself)
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+
+            if (_instance == null)
+            {
+                Debug.Log("CAN YOU FIND THE INSTANCE OF THIS OBJECT");
+                _instance = GameObject.FindObjectOfType<T>();
+                if (_instance == null)
+                {
+                    Debug.Log("Create new object instance");
+                    GameObject go = new GameObject("[Singleton] ? Manager");
+                    _instance = go.AddComponent<T>();
+
+                }
+            }
+            return _instance;
+        }
+
+    }
+
+    protected virtual void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}

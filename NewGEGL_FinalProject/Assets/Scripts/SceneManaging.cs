7627f6febba4f6b079c8a23f36059030dac3a04a
EyepatchDoge <45701926+EyepatchDoge@users.noreply.github.com>
2019-12-12 13:49:47 -0500

    Updated Pause Menu
    ~Made pause menu accessible with p
    ~

--- NewGEGL_FinalProject/Assets/Scripts/SceneManaging.cs
+++ NewGEGL_FinalProject/Assets/Scripts/SceneManaging.cs
@@ -6,7 +6,20 @@ using UnityEngine.SceneManagement;
 public class SceneManaging : MonoBehaviour
 {
     public string newScene;
-    
+    public GameObject pauseScreen;
+    public CameraMovement camMove;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            pauseScreen.SetActive(true);
+            //Time.timeScale = 0;
+            GamePaused();
+            
+        }
+    }
+
     public void ChangeScene()
     {
         SceneManager.LoadScene(newScene);
@@ -21,10 +34,23 @@ public class SceneManaging : MonoBehaviour
     public void GamePaused()
     {
         Time.timeScale = 0;
+        if(camMove != null)
+        {
+            camMove.enabled = false;
+        }
     }
 
     public void GameResumed()
     {
         Time.timeScale = 1;
+        if (camMove != null)
+        {
+            camMove.enabled = true;
+        }
+    }
+
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name/*.ToString()*/);
     }
 }
--- NewGEGL_FinalProject/Assets/Scripts/SoundManager.cs
+++ NewGEGL_FinalProject/Assets/Scripts/SoundManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
-public class SoundManager : MonoBehaviour
+public class SoundManager : SingletonManager<SoundManager>
 {
     //public AudioSource sAudio;
     public static SoundManager instance;
